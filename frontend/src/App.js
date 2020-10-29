68269a6cd2e8a15b55d9f5d1a7f30ac156fa3fe4
Gabriel <gbryan6@hotmail.com>
2020-10-29 17:54:09 -0300

    feat: initial structure

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>Initial Structure Bairs</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
