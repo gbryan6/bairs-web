da40bcc48833f813000750391b601b3e5727ed89
Gabriel <gbryan6@hotmail.com>
2020-10-29 21:16:09 -0300

    feat: building a home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
+import React from 'react';
+import Home from './pages/home';
 
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <code>Initial Structure Bairs</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Home/>
   );
 }
 
---
+++ frontend/src/pages/home/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './index.css';
+import Header from './components/header';
+
+function Home() {
+    return(
+        <>
+        <div className='home__content'>
+            <header className='top__bar'>
+                <Header />
+            </header>
+            <section>
+                Aqui vai ficar o feed
+            </section>
+        </div>
+        </>
+    )
+}
+
+export default Home;
